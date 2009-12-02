f8a5807872e13d43a31a282db93f1c3bb953caca
Jarrod Roberson <jarrod@vertigrated.com>
2009-12-02 01:18:33 +0100

    updating response processing code with the code from pichi making it much more functional in nature Conflicts:
    	src/inet_mdns.erl

--- src/inet_mdns.erl
+++ src/inet_mdns.erl
@@ -6,7 +6,7 @@
 -export([stop/1,receiver/1]).
 -export([subscribe/2,unsubscribe/2,getsubs/1]).
 
-% gets a timestamp in ms from the epoch
+% gets a timestamp in ms from the epoch 1970-01-01
 get_timestamp() ->
     {Mega,Sec,Micro} = erlang:now(),
     (Mega*1000000+Sec)*1000000+Micro.
@@ -66,16 +66,17 @@ process_dnsrec(Sub,{error,E}) ->
     Sub;
 process_dnsrec(Sub,{ok,#dns_rec{anlist=Responses}}) ->
     dict:map(fun(S, V) -> process_responses(S, V, Responses) end, Sub).
-
+ 
 % process the list of resource records one at a time
 process_responses(S, Value, Responses) ->
     lists:foldl(fun(#dns_rr{domain = Domain} = Response, Val) ->
         process_response(lists:suffix(S, Domain), Response, Val)
     end, Value, Responses).
-
+ 
 process_response(false, _Response, Val) -> Val;
 process_response(true, #dns_rr{ttl = TTL} = Response, Val) when TTL == 0 ->
     dict:new();
-process_response(true, #dns_rr{domain = Domain, type = Type, class = Class} = Response) ->
+process_response(true, #dns_rr{domain = Domain, type = Type, class = Class} = Response, Val) ->
     NewRR = Response#dns_rr{tm=get_timestamp()},
     dict:store({Domain, Type, Class}, NewRR, Val).
+
